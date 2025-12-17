1b16baacba4f3336480a65df6e14293920f68594
Guillaume Legros <guillaume.legros@argil.ai>
2025-12-17 18:51:30 +0100

    fix: footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,8 @@
 class MainFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div class="max-w-7xl mx-auto mt-24 pt-8 border-t border-white/10 text-center text-xs text-gray-500 uppercase tracking-widest">
-            © 2024 Maître Clothilde Legros - Mentions Légales - Barreau de Paris
+        <div class="max-w-7xl mx-auto pt-8 border-t border-white/10 text-center text-xs text-white/70 uppercase tracking-widest">
+            © 2024 MAÎTRE CLOTHILDE LEGROS - MENTIONS LÉGALES - BARREAU DE PARIS
         </div>`;
     }
 }
