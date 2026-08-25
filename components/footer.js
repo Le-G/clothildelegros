1dafada1558d20c7cc383d9aedf9ee54a070fd70
Guillaume Legros <guillaume.legros@argil.ai>
2026-08-25 14:08:22 +0200

    feat: add SIREN and VAT numbers to footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,9 @@
 class MainFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div class="max-w-7xl mx-auto pt-8 border-t border-white/10 text-center text-xs text-white/70 uppercase tracking-widest">
-            © 2026 MAÎTRE CLOTHILDE LEGROS - MENTIONS LÉGALES - BARREAU DE PARIS
+        <div class="max-w-7xl mx-auto pt-8 border-t border-white/10 text-center text-xs text-white/70 uppercase tracking-widest space-y-3">
+            <p>© 2026 MAÎTRE CLOTHILDE LEGROS - MENTIONS LÉGALES - BARREAU DE PARIS</p>
+            <p class="tracking-wider">SIREN 911 854 974 — TVA FR34 911 854 974</p>
         </div>`;
     }
 }
